379690a1832d1e95fce67c62c302be307835683d
Cristopher-2003 <martinezcristopher589@gmail.com>
2023-02-17 16:11:56 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,244 @@
+import React , {useState} from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ModalBody, ModalHeader, ModalFooter, Modal } from 'reactstrap';
+
+function App(){
+  const dataPersonas = [
+  {id: 1 , nombre: "Cristian" , descripcion : "cantante"},
+  {id: 2 , nombre: "Andres" , descripcion : "futbolista"},
+  {id: 3 , nombre: "Felipe" , descripcion : "albañil"},
+  {id: 4 , nombre: "Lorena" , descripcion : "modelo"},
+  {id: 5 , nombre: "Cristina" , descripcion : "auditora"},
+  {id: 6 , nombre: "Romina" , descripcion : "secretaria"},
+  {id: 7 , nombre: "Carlos" , descripcion : "programador"},
+  {id: 8 , nombre: "Esteban" , descripcion : "limpiador"},
+  {id: 9 , nombre: "Robin" , descripcion : "cargador"},
+  {id: 10 , nombre: "Sara" , descripcion : "cocinero"},
+];
+
+
+const [data , setData] = useState(dataPersonas);
+const [modalEditar , setModalEditar] = useState(false);
+const [modalEliminar , setModalEliminar] = useState(false);
+const [modalInsertar , setModalInsertar] = useState(false);
+
+const [personaSeleccionada , setPersonaSeleccionada] = useState({
+  id:"",
+  nombre:"",
+  descripcion:""
+});
+ 
+const seleccionarPersona = (elemento , caso) => {
+  setPersonaSeleccionada(elemento);
+  (caso === "Editar")?setModalEditar(true):setModalEliminar(true)}
+
+const handleChange=e=>{
+  const {name,value} = e.target;
+  setPersonaSeleccionada((prevState) => ({
+    ...prevState,
+    [name]:value
+  }));
+}
+
+const editar=() =>{
+  var dataNueva=data;
+  dataNueva.map(persona=>{
+    if (persona.id===personaSeleccionada.id){
+      persona.descripcion=personaSeleccionada.descripcion;
+      persona.nombre=personaSeleccionada.nombre;
+    }
+  });
+  setData(dataNueva);
+  setModalEditar(false);
+}
+
+const eliminar =()=>{
+  setData(data.filter(persona=>persona.id!==personaSeleccionada.id));
+  setModalEliminar(false);
+}
+
+const abrirModalInsertar=()=>{
+  setPersonaSeleccionada(null);
+  setModalInsertar(true);
+}
+
+const insertar =() => {
+  var valorInsertar = personaSeleccionada;
+  valorInsertar.id=data[data.length-1].id+1;
+  var dataNueva=data;
+  dataNueva.push(valorInsertar);
+  setData(dataNueva);
+  setModalInsertar(false);
+}
+
+return(
+  <div className="App">
+    <header>
+      <h2>Lista de Personas</h2>
+    </header>
+    <br/>
+    <button className='btn btn--success' onClick={() => abrirModalInsertar()}>Insertar</button>
+    <br/>  <br/>
+    <div id="main-container">
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Descripcion</th>
+            <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+          {data.map(elemento=>(
+            <tr>
+              <td>{ elemento.id} </td>
+              <td>{ elemento.nombre} </td>
+              <td>{ elemento.descripcion} </td>
+              <td> 
+              <button className='btn btn-primary' onClick={()=> seleccionarPersona(elemento, "Editar")}>Editar</button> {""} 
+              <button className='btn btn-danger' onClick={()=> seleccionarPersona(elemento, "Eliminar")}>Eliminar</button>
+              </td>
+            </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+
+
+    <Modal isOpen={modalEditar}>
+      <ModalHeader>
+        <div>
+          <h3>Editar Datos</h3>
+        </div>
+      </ModalHeader>
+      <ModalBody>
+        <div className='form-group'>
+          <label>Id</label>
+          <input
+          className='form-control'
+          type="text"
+          name='id'
+          value={personaSeleccionada && personaSeleccionada.id}
+          />
+          <br/>
+
+
+          <label>Persona</label>
+          <input
+          className='form-control'
+          type="text"
+          name='nombre'
+          value={personaSeleccionada && personaSeleccionada.nombre}
+          onChange={handleChange}
+          />
+          <br/>
+
+          <label>Descripcion</label>
+          <input
+          className='form-control'
+          type="text"
+          name='descripcion'
+          value={personaSeleccionada && personaSeleccionada.descripcion}
+          onChange={handleChange}
+          />
+          <br/>
+          </div>
+      </ModalBody>
+      
+      
+      <ModalFooter>
+        <button className='btn btn-primary' onClick={()=> editar()}>
+          Actualizar
+        </button>
+        <button
+        className='btn btn-danger'
+        onClick={()=> setModalEditar(false)}>
+          Cancelar
+        </button>
+      </ModalFooter>
+    </Modal>
+
+
+    <Modal isOpen={modalEliminar}>
+      <ModalBody>
+      Estas seguro que deseas eliminar a la persona {personaSeleccionada && personaSeleccionada.nombre}
+      </ModalBody>
+      <ModalFooter>
+        <button className='btn btn-danger' onClick={()=> eliminar()}>
+          Si
+        </button>
+        <button
+        className='btn btn-secondary'
+        
+        onClick={()=> setModalEliminar(false)}>
+          No
+        </button>
+      </ModalFooter>
+    </Modal>
+
+
+
+    <Modal isOpen={modalInsertar}>
+      <ModalHeader>
+        <div>
+          <h3>Insertar Persona</h3>
+        </div>
+      </ModalHeader>
+      
+      <ModalBody>
+        <div className='form-group'>
+          <label>Id</label>
+          <input 
+          className="form-control"
+          type="text"
+          name="id"
+          value = {data[data.length-1].id+1}
+          />
+          <br/>
+
+          <label>Nombre</label>
+          <input 
+          className="form-control"
+          type="text"
+          name="nombre"
+          value = {personaSeleccionada ? personaSeleccionada.nombre:""}
+          onChange = {handleChange}
+          />
+          <br/>
+
+          
+          <label>Descripcion</label>
+          <input
+          className='form-control'
+          type="text"
+          name='descripcion'
+          value={personaSeleccionada ? personaSeleccionada.descripcion:""}
+          onChange = {handleChange}
+          />
+          <br/>
+        </div>
+      </ModalBody>
+      
+      
+      
+      <ModalFooter>
+        <button className='btn btn-primary' onClick={()=> insertar()}>
+          Insertar
+        </button>
+        
+        <button className='btn btn-danger' onClick={()=> setModalInsertar(false)}
+        >
+          Cancelar
+        </button>
+      </ModalFooter>
+  </Modal>
+</div> 
+
+);
+}
+  
+
+
+export default App;
